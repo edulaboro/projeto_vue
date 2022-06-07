c0fb7e6a83b5c99ec031482674bd2815fa8d23f7
Eduardo Ferreira <esferreira.tecno@gmail.com>
2022-06-07 20:11:37 -0300

    Finalizado projeto Vue

--- js/script.js
+++ js/script.js
@@ -7,12 +7,72 @@ const app = Vue.createApp({
                 cambio:false,
                 diaSemana:false,
                 desconto:false
-            }
+            }, 
+            mensagem: {
+                idade : "Cálculo da idade a partir do ano de nascimento inserido pelo usuário",
+                cambio: "Cálculo do valor em reais a partir de um valor em dólar e cotação inserida pelo usuário",
+                diaSemana : "Exibir o dia da semana a partir de uma data inserida pelo usuário",
+                desconto : "Calcular  o  resultado  a  partir  de  um  valor  base  e  o percentual de desconto indicados pelo usuário"
+            },
+            textoInicial:'Olá, Seja Bem Vindo ao Nosso Sistema',
+            campoIdade:'',
+            resultado:'',
+            info:'',
+            dolar:'',
+            cotacao:'',
+            data:''
         }
     },
     methods:{
         verificarLink(classe){
-            console.log(classe)
+            //console.log(classe[1])
+            let itens = classe[1]// classe é um parâmetro que irá conter um vetor com as classes da tag na qual a função está sendo chamada.
+
+            // for..in permite percorrer um objeto
+            for(elementos in this.bloco){
+                //console.log(elementos)
+                if(itens == elementos){
+                    this.bloco[elementos] = true
+                    this.textoInicial = this.mensagem[elementos]
+                    this.resultado = "" //Resetando as informações
+                    this.info=""
+                }
+                else{
+                    this.bloco[elementos] = false
+                }
+            }
+        },
+        calcularIdade(){
+            let anoAtual = new Date().getFullYear()// Estou pegando o ano atual
+            if(this.validarIdade()){
+                let resposta = anoAtual - this.campoIdade
+                //console.log(resposta)
+                this.resultado = `Você possui ${resposta} anos` 
+            }
+            
+        },
+        validarIdade(){
+            if(this.campoIdade < 1900 || this.campoIdade > 2050){
+                this.info = "Você precisa informar um valor entre 1900 e 2050"
+                this.resultado = ""// Irá retirar a mensagem do cálculo da idade
+                return false// Significa que o usuário inseriu um valor errado
+            }
+            else{
+               this.info=""
+                return true// Significa que o usuário inseriu um valor correto
+            }
+        },
+        converterDolar(){
+            let padrao = /^[0-9]+(\.([0-9]{2}))?$/ //Criando uma expressão regular
+            if(padrao.test(this.dolar) && padrao.test(this.cotacao)){
+                let resposta = this.dolar * this.cotacao
+                this.resultado = `U$${this.dolar} convertido para Real é R$${resposta}`
+                this.info=""// Retirando a mensagem de erro após informar resultado correto
+            }
+            else{
+                this.info = "Informe apenas números inteiros ou separados por ponto com 2 casas decimais"
+                this.resultado=""
+            }
         }
     }
 })
